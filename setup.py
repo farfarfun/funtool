f0d32b163b170528d4aa81c1715b40b41e160f57
farfarfun <farfarfun@qq.com>
2026-08-27 00:59:50 +0800

    build: 迁移到 pyproject.toml，移除 setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from funpypi import setup
-
-install_requires = ["cryptography", "funsecret>=1.4.84"]
-
-setup(
-    package_name="farfuntool",
-    name="funtool",
-    install_requires=install_requires,
-)
